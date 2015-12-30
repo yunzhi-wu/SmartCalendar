eb623e596290db2d3c72b50903b87f0c7c0bcec5
Yunzhi_Wu <wuyunzhizhi@gmail.com>
2015-12-30 18:29:54 +0100

    Use argument --statistic to show time resources
    Add 'time budget' in configure file, to configure hours used for a
    project per week.

--- smart_calendar.py
+++ smart_calendar.py
@@ -12,6 +12,7 @@ import configparser
 from event import Event
 import copy
 import datetime
+from operator import itemgetter
 
 debug_level = 0
 debug_level_info = 1
@@ -27,6 +28,8 @@ try:
                         help="increase output verbosity")
     parser.add_argument("-d", "--debug", action="store_true",
                         help="display debug information")
+    parser.add_argument("-s", "--statistic", action="store_true",
+                        help="display projects and their attributes")
     flags = parser.parse_args()
     if flags.info:
         debug_level = debug_level_info
@@ -40,6 +43,29 @@ except ImportError:
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'auth\client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
+def get_available_week_hours():
+    """
+    24 hours a day:
+    sleeping: 8 hours
+    in office: 8.5 hours
+    way to and from office: 2 hours
+    breakfast: 0.5 hours
+    dinner: 1 hours
+
+    Returns:
+
+    """
+    work_day_hours = 24 - 8.5 - 8 - 2 - 0.5 - 1
+    weekend_day_hours = 24 - 8 - 0.5 - 1 - 1
+    week_hours = work_day_hours * 5 + weekend_day_hours * 2
+
+    print_level(debug_level_info, 'work_day_hours: {0} weekend_day_hours = {1}'
+                .format(work_day_hours, weekend_day_hours))
+    print_level(debug_level_info, 'week_hours: {0}'
+                .format(week_hours))
+    return week_hours
 
 
 def create_event(service, events):
@@ -108,20 +134,21 @@ def find_available_periods(events):
         d1 = datetime.datetime.strptime(event['start'].get('dateTime'), fmt)
         d2 = datetime.datetime.strptime(event['end'].get('dateTime'), fmt)
         if (d2 - d1) > datetime.timedelta(hours=6):
-            print("There is a period longer than 6 hours: {0}  -  {1} : {2}".format(event['start'].get('dateTime'),
-                                                                                    event['end'].get('dateTime'),
-                                                                                    event['summary']))
+            print("There is a period longer than 6 hours: {0}  -  {1} : {2}"
+                  .format(event['start'].get('dateTime'),
+                          event['end'].get('dateTime'),
+                          event['summary']))
             want_to_ignore = input("Do you want to ignore it? [Yes] ")
             if want_to_ignore in ['', 'y', 'Y', 'yes', 'Yes', 'YES']:
                 print("This event is ignored when to assign tasks")
                 continue
         used_periods.append([d1, d2])
-    print("Used periods:")
+    print_level(debug_level_info, "Used periods:")
     for period in used_periods:
-        print(period)
+        print_level(debug_level_info, period)
 
+    merged_periods = []
     if used_periods:
-        merged_periods = []
         start, end = used_periods[0]
         # merge possible overlapped period
         for start_itr, end_itr in used_periods[1:]:
@@ -131,13 +158,13 @@ def find_available_periods(events):
             elif end < end_itr:  # overlap, but the previous one end earlier
                 end = end_itr
         merged_periods.append([start, end])
-    print('Merged periods:')
+    print_level(debug_level_info, 'Merged periods:')
     for period in merged_periods:
-        print(period)
+        print_level(debug_level_info, period)
 
     today = datetime.date.today()
-    morning = datetime.time(7,  0 ,0, 0)
-    evening = datetime.time(22, 0 ,0, 0)
+    morning = datetime.time(7,  0, 0, 0)
+    evening = datetime.time(22, 0, 0, 0)
 
     today_start = datetime.datetime.combine(today, morning)
     today_end = datetime.datetime.combine(today, evening)
@@ -155,9 +182,9 @@ def find_available_periods(events):
         end = today_end
         available_periods.append([start, end])
 
-    print('Available periods')
+    print_level(debug_level_info, 'Available periods')
     for period in available_periods:
-        print(period)
+        print_level(debug_level_info, period)
     return available_periods
 
 
@@ -204,7 +231,7 @@ def scheduling(available_periods, projects):
             else:
                 period_assign = copy.copy(period)
                 period = []
-            print("scheduling: selected period: {0}", period_assign)
+            print_level(debug_level_info, "scheduling: selected period: {0}".format(period_assign))
 
             emergency = 0
             importance = 0
@@ -254,22 +281,89 @@ def print_level(level, *argv):
         print(*argv)
 
 
+def show_project_attributes(projects):
+    total_hours = 0
+    projects_attributes = {}
+    for project in projects:
+        attributes = {key: value for (key, value) in projects.items(project)}  # setup a dictionary
+        if attributes.get('time budget'):
+            total_hours += float(attributes.get('time budget'))
+        projects_attributes[project] = attributes
+
+    print_level(debug_level_info, 'Total hours needed per week: {0}'.format(total_hours))
+
+    sorted_projects = []
+    max_percentage = 0
+    for project in projects:
+        attributes = projects_attributes[project]
+        if attributes.get('time budget'):
+            time_budget = float(attributes.get('time budget'))
+            time_percentage = int(time_budget / total_hours * 100)
+            if max_percentage < time_percentage:
+                max_percentage = time_percentage
+            attributes['time percentage'] = time_percentage
+            sorted_projects.append([project,attributes.get('emergency'), attributes.get('importance')])
+    sorted_projects.sort(key=itemgetter(1, 2), reverse=True)
+    print_level(debug_level_debug, 'Sorted_projects:')
+    print_level(debug_level_debug, sorted_projects)
+    print_level(debug_level_debug, '\n')
+
+    for item in sorted_projects:
+        project = item[0]
+        attributes = projects_attributes[project]
+        time_budget = attributes.get('time budget')
+        time_percentage = attributes.get('time percentage')
+        emergency = attributes.get('emergency')
+        importance = attributes.get('importance')
+
+        if time_percentage:
+            tmp = time_percentage / max_percentage
+            if tmp < 0.1:
+                prefix = '*         '
+            elif tmp < 0.2:
+                prefix = '**        '
+            elif tmp < 0.3:
+                prefix = '***       '
+            elif tmp < 0.4:
+                prefix = '****      '
+            elif tmp < 0.5:
+                prefix = '*****     '
+            elif tmp < 0.6:
+                prefix = '******    '
+            elif tmp < 0.7:
+                prefix = '*******   '
+            elif tmp < 0.8:
+                prefix = '********  '
+            elif tmp < 0.9:
+                prefix = '********* '
+            else:
+                prefix = '**********'
+
+            print_level(debug_level_info, '{0} {1:02d}% ({2:02d}/{3:02d}) of these time is for: {4} [{5},{6}]'
+                        .format(prefix, time_percentage, int(time_budget), int(total_hours),
+                                project, emergency, importance))
+
+
 def main():
     """
     Shows basic usage of the Google Calendar API.
     Get the upcoming ten events
     Insert an event
     """
-    global debug_level
+    projects = get_projects()
+
+    for project in projects:
+        print_level(debug_level_debug, projects.items(project), project)
+
+    if flags.statistic:
+        get_available_week_hours()
+        show_project_attributes(projects)
+        return
 
     service = get_service()
     events = get_event(service)
 
     available_periods = find_available_periods(events)
-
-    projects = get_projects()
-    for project in projects:
-        print(projects.items(project), project)
 
     # TODO: merge events and projects into a unified database
 
